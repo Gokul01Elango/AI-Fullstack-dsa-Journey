ba0e1fd4fd1099a6c5050fb38151cead9a1dea76
zero <zero@zero.zero>
2025-07-06 07:21:50 +0530

    Basic Concepts Learning

---
+++ dsa/basic/Sample.py
@@ -0,0 +1,9 @@
+#My name is Gokul, I am 22 years old, 178.5 cm tall, and learning Python: True
+name="Gokul"
+age = 22
+height= 178.5
+language = "Python"
+IsEasy = True
+
+print("My Name is ",name,"I am ",age,
+"Years old",height," cm tall , and learning ",language," : ",IsEasy)
